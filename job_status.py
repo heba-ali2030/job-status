58707d3455a3b7e5afc6f7dbaf37e9aef91aa599
heba-ali2030 <heba.galaa@gmail.com>
2021-02-04 14:09:59 -0800

    job_status.py

---
+++ job_status.py
@@ -0,0 +1,9 @@
+# organization: assiut University
+name = input('what is your faculty branch?')
+print(f'{name} get a draft doc. from mr. ahmed')
+
+job = input('what is your title ?')
+print(f'{job} get 3 stamps')
+
+master = input ('title of your master thesis')
+duration = input(' is it finished?')
