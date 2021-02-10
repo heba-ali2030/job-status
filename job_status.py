2c8598d517500649953b0078b03f7b600f52017a
heba-ali2030 <heba.galaa@gmail.com>
2021-02-10 23:06:21 +0200

    add

--- job_status.py
+++ job_status.py
@@ -1,9 +1,37 @@
 # organization: assiut University
-name = input('what is your faculty branch?')
-print(f'{name} get a draft doc. from mr. ahmed')
+name = input('what is your faculty name? :')
+if name == 'vet medicine': 
+    print(f' you should get a draft doc. from mr. ahmed')
+elif name == 'medicine': 
+    print(f' you should get a draft doc. from ms. Ola')
+elif name =='commerce': 
+    print(f' you should get a draft doc. from mr. Mahmoud')
+elif name == 'Education': 
+    print(f' you should get a draft doc. from miss. Yasmin') 
+else: 
+    print(f' you should call eng. Hossam immediately because he is the director and responsible for solving your problem and achieving your dreams')
 
-job = input('what is your title ?')
-print(f'{job} get 3 stamps')
+job = input('what is your  job title ?: ')
+if job == 'demonstrator': 
+    print(f' you should get get 3 stamps')
+elif job == 'assistant lecturer': 
+    print(f' you should get get 5 stamps')
+else:
+    print(f' you should get 10 stamps and pay 100$ to support Egypt')
 
-master = input ('title of your master thesis')
-duration = input(' is it finished?')
+master = input ('please write title of your master thesis : ')
+
+duration = input(' do you finished it (yes/no) : ')
+if duration == 'yes': 
+    print(f' congratulations')
+elif duration == 'no': 
+    print(f' God with you ')
+else:
+    print(f' Good luck')
+
+print(f' please write down your signatur below')
+print(f' You can take the stamped job status after 2 days  before 12.00 pm, Have a nice day')
+
+signature= input('signature : ')
+
+
